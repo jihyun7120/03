0b805d383086339c5e013c4a2abc81b7b88c94d4
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-09-21 21:03:56 +0900

    02

--- main.c
+++ main.c
@@ -3,16 +3,15 @@
 // ctrl s 할 때 저장은 그 기본 폴더에 하기(그게뜰거야) 
 int main(int argc, char *argv[])
 {
-    int input_int; //input_int라는 변수 생성 
-    float input_float;//마찬가지롯생성 
+    char c;
+    int i;
     
-    printf("enter an integer: "); 
-    scanf("%d", &input_int);//변수입력받기.&붙이는거잊지말기
-    
-     printf("enter a float: "); 
-    scanf("%f", &input_float);
-    
-    printf("integer : %d, float : %f\n", input_int, input_float);
+    printf("input a number: ");
+    scanf("%c", &c);//char 형태로 받을거니까 %c야  
+    //char 형태로 받아서 숫자로 변환시킬거니까 
+    i = c - '0';//c저장된 문자를 숫자로 변환 by 아스키코드
+     
+    printf("The number is %i\n",i);
      
   system("PAUSE");
   return 0;
