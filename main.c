032f48b2d4575d99b5f3f5aa2d29b6e193167d91
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-09-21 21:22:34 +0900

    03

--- main.c
+++ main.c
@@ -3,16 +3,15 @@
 // ctrl s 할 때 저장은 그 기본 폴더에 하기(그게뜰거야) 
 int main(int argc, char *argv[])
 {
-    char c;
-    int i;
+    char c, d;
     
-    printf("input a number: ");
-    scanf("%c", &c);//char 형태로 받을거니까 %c야  
-    //char 형태로 받아서 숫자로 변환시킬거니까 
-    i = c - '0';//c저장된 문자를 숫자로 변환 by 아스키코드
-     
-    printf("The number is %i\n",i);
-     
-  system("PAUSE");
+    printf("input a character: ");
+    scanf("%c", &c);
+    
+    d = c + 1;
+
+    printf("the next character of %c(%d) is %c(%d)\n", c, c, d, d); 
+    
+    system("PAUSE");
   return 0;
 }
